9e36c7e6ec4ef80a16851d97e68d7291dcb3c1e4
Lars Nieuwenhuizen <l.nieuwenhuizen@developers.nl>
2016-10-29 18:24:32 +0200

    TASK: Update public webroot generator file

--- bin/generate-public-link.sh
+++ bin/generate-public-link.sh
@@ -1,5 +1,35 @@
 #!/bin/bash
 
+function generateForFlow() {
+    echo "Symlink for Flow is created"
+    ln -s ./app/Web/ Web
+}
+function generateForLaravel() {
+    echo "Symlink for Laravel is created"
+    ln -s ./app/public/ Web
+}
+function generateForSymfony() {
+    echo "Symlink for Symfony is created"
+    ln -s ./app/web/ Web
+}
+
+if  [ $1 == "Laravel" ]
+    then
+        generateForLaravel
+    exit
+fi
+if [ $1 == "Symfony" ]
+    then
+        generateForSymfony
+    exit
+fi
+if [ $1 == "Flow" ]
+    then
+        generateForFlow
+    exit
+fi
+
+
 echo 'This generates a symlink to the public directory, choose your framework'
 
 PS3='Please enter your choice: '
@@ -8,18 +38,15 @@ select opt in "${options[@]}"
 do
     case $opt in
         "Flow")
-            echo "Symlink for Flow is created"
-            ln -s ./app/Web/ Web
+            generateForFlow
             exit
             ;;
         "Laravel")
-            echo "Symlink for Laravel is created"
-            ln -s ./app/public/ Web
+            generateForLaravel
             exit
             ;;
         "Option 3")
-            echo "Symlink for Symfony is created"
-            ln -s ./app/web/ Web
+            generateForSymfony
             exit
             ;;
         "None of the above")
